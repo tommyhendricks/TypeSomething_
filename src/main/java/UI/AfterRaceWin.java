5023df74fe734c0c7b7daad83f59f4b04e5ca523
Tommy Hendricks <Tommy Hendricks@10.181.135.231>
2021-04-07 23:59:49 -0400

    Small Additions - The time feature in the AfterRaceWin is now working properly.

--- src/main/java/UI/AfterRaceWin.java
+++ src/main/java/UI/AfterRaceWin.java
@@ -26,11 +26,43 @@ public class AfterRaceWin extends javax.swing.JFrame {
      * This will set the info on the After race screen.
      * 
      */
-    public void setInfo(float WPM, int Errors, float time, float percentage){
+    public void setInfo(float WPM, int Errors, int time, float percentage){
+        int seconds = 0;
+        int minutes = 0;
+        String s2 = null;
+        String s1 = null;
+        
+        time = 75;
+        
         this.LastRaceSpeed.setText(String.format("%.0f", WPM));
         this.LastRaceErrors.setText(String.valueOf(Errors));
-        this.LastRacePercentage.setText(String.valueOf(percentage) + '%');
+        this.LastRacePercentage.setText(String.format("%.0f", percentage) + '%');
         
+        if(time > 60){
+            minutes = time / 60;
+            time = time % 60;
+            seconds = time;
+        }
+        else{
+            minutes = 0;
+            seconds = time;
+        }
+        
+        if(minutes < 10 && minutes != 0)
+            s1 = '0' + String.valueOf(minutes);
+        else if(minutes == 0)
+            s1 = "00";
+        else
+            s1 = String.valueOf(minutes);
+        
+        if(seconds < 10 && seconds != 0)
+            s2 = '0' + String.valueOf(seconds);
+        else if(seconds == 0)
+            s2 = "00";
+        else
+            s2 = String.valueOf(seconds);
+        
+        this.LastRaceTime.setText(s1 + ':' + s2);
     }
     
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -39,15 +71,15 @@ public class AfterRaceWin extends javax.swing.JFrame {
         jTextField7 = new javax.swing.JTextField();
         jTextField8 = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
-        SpeedLable = new javax.swing.JTextField();
         LastRaceSpeed = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         LastRaceErrors = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
         LastRacePercentage = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
         LastRaceTime = new javax.swing.JTextField();
-        jTextField10 = new javax.swing.JTextField();
+        WPMLabel = new javax.swing.JLabel();
+        ErrorsLabel = new javax.swing.JLabel();
+        PercentageLabel = new javax.swing.JLabel();
+        TimeLabel = new javax.swing.JLabel();
 
         jTextField7.setBackground(new java.awt.Color(0, 0, 0));
         jTextField7.setFont(new java.awt.Font("Yu Gothic Light", 0, 20)); // NOI18N
@@ -63,11 +95,7 @@ public class AfterRaceWin extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        SpeedLable.setEditable(false);
-        SpeedLable.setBackground(new java.awt.Color(0, 0, 0));
-        SpeedLable.setForeground(new java.awt.Color(255, 255, 255));
-        SpeedLable.setText("Speed:");
-
+        LastRaceSpeed.setEditable(false);
         LastRaceSpeed.setBackground(new java.awt.Color(0, 0, 0));
         LastRaceSpeed.setFont(new java.awt.Font("Yu Gothic Light", 0, 20)); // NOI18N
         LastRaceSpeed.setForeground(new java.awt.Color(255, 255, 255));
@@ -81,38 +109,38 @@ public class AfterRaceWin extends javax.swing.JFrame {
             }
         });
 
+        LastRaceErrors.setEditable(false);
         LastRaceErrors.setBackground(new java.awt.Color(0, 0, 0));
         LastRaceErrors.setFont(new java.awt.Font("Yu Gothic Light", 0, 20)); // NOI18N
         LastRaceErrors.setForeground(new java.awt.Color(255, 255, 255));
         LastRaceErrors.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         LastRaceErrors.setText("N/A");
 
-        jTextField4.setEditable(false);
-        jTextField4.setBackground(new java.awt.Color(0, 0, 0));
-        jTextField4.setForeground(new java.awt.Color(255, 255, 255));
-        jTextField4.setText("Errors:");
-
+        LastRacePercentage.setEditable(false);
         LastRacePercentage.setBackground(new java.awt.Color(0, 0, 0));
         LastRacePercentage.setFont(new java.awt.Font("Yu Gothic Light", 0, 20)); // NOI18N
         LastRacePercentage.setForeground(new java.awt.Color(255, 255, 255));
         LastRacePercentage.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         LastRacePercentage.setText("N/A");
 
-        jTextField6.setEditable(false);
-        jTextField6.setBackground(new java.awt.Color(0, 0, 0));
-        jTextField6.setForeground(new java.awt.Color(255, 255, 255));
-        jTextField6.setText("Percentage:");
-
+        LastRaceTime.setEditable(false);
         LastRaceTime.setBackground(new java.awt.Color(0, 0, 0));
         LastRaceTime.setFont(new java.awt.Font("Yu Gothic Light", 0, 20)); // NOI18N
         LastRaceTime.setForeground(new java.awt.Color(255, 255, 255));
         LastRaceTime.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         LastRaceTime.setText("N/A");
 
-        jTextField10.setEditable(false);
-        jTextField10.setBackground(new java.awt.Color(0, 0, 0));
-        jTextField10.setForeground(new java.awt.Color(255, 255, 255));
-        jTextField10.setText("Time:");
+        WPMLabel.setForeground(new java.awt.Color(255, 255, 255));
+        WPMLabel.setText("WPM:");
+
+        ErrorsLabel.setForeground(new java.awt.Color(255, 255, 255));
+        ErrorsLabel.setText("Errors:");
+
+        PercentageLabel.setForeground(new java.awt.Color(255, 255, 255));
+        PercentageLabel.setText("Percentage:");
+
+        TimeLabel.setForeground(new java.awt.Color(255, 255, 255));
+        TimeLabel.setText("Time:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -120,47 +148,48 @@ public class AfterRaceWin extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(SpeedLable)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(WPMLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(LastRaceSpeed, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(LastRaceErrors, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
-                    .addComponent(jTextField4))
+                    .addComponent(ErrorsLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(LastRacePercentage)
-                            .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(PercentageLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(LastRaceTime, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)
-                            .addComponent(jTextField10))))
+                        .addComponent(TimeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton1))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(LastRacePercentage, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(LastRaceTime, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)))
                 .addGap(24, 24, 24))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(SpeedLable, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(LastRaceSpeed, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(WPMLabel)
+                            .addComponent(ErrorsLabel)
+                            .addComponent(PercentageLabel)
+                            .addComponent(TimeLabel))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(LastRaceSpeed, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(LastRaceErrors, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextField10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(53, 53, 53)
                         .addComponent(LastRaceTime, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(53, 53, 53)
                         .addComponent(LastRacePercentage, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
@@ -186,16 +215,16 @@ public class AfterRaceWin extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel ErrorsLabel;
     private javax.swing.JTextField LastRaceErrors;
     private javax.swing.JTextField LastRacePercentage;
     private javax.swing.JTextField LastRaceSpeed;
     private javax.swing.JTextField LastRaceTime;
-    private javax.swing.JTextField SpeedLable;
+    private javax.swing.JLabel PercentageLabel;
+    private javax.swing.JLabel TimeLabel;
+    private javax.swing.JLabel WPMLabel;
     private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField10;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField8;
     // End of variables declaration//GEN-END:variables
